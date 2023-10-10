c505bb8d35b8681f21d32923af2033fb6237e3e0
umair-avalor <umair@avalor.investments>
2023-10-10 23:44:06 +0530

    added moving grads on auth pages

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -56,6 +56,9 @@ export default function AuthenticationPage() {
         </div>
         <div className="lg:p-8 w-full">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex justify-center text-lg font-medium lg:hidden ">
+              <Image src={logo} alt="logo" />
+            </div>
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Create an account
