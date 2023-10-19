31355cb03b8fd6ee18712ce909815955c568c9b9
SD-MALINGA <somyaditya@decentralisedinvestment.group>
2023-10-19 16:36:05 +0530

    user page

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -15,3 +15,19 @@ export const AuthPost = async (path: string, data: any): Promise<any> => {
     return error.response;
   }
 };
+
+export const AuthGet = async (path: string): Promise<any> => {
+  try {
+    const userData = localStorage.getItem("user") || "";
+    const parsedUserData = JSON.parse(userData);
+    const token = parsedUserData.token;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    };
+    const res = await axios.get(path, { headers });
+    return res;
+  } catch (error: any) {
+    return error.response;
+  }
+};
