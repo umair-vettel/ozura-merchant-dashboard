1fb72030491dbeceed37b267accf80350ac09920
SD-MALINGA <somyaditya@decentralisedinvestment.group>
2023-10-20 01:43:49 +0530

    build fix

--- src/app/(dashboard)/users/[id]/page.tsx
+++ src/app/(dashboard)/users/[id]/page.tsx
@@ -1,4 +1,4 @@
-import UserDetailsPage from "@/components/dashboard/Users/UserDetailsPage";
+import UserDetailsPage from "@/components/dashboard/users/UserDetailsPage";
 
 export default function DemoPage({ params }: any) {
   return (
