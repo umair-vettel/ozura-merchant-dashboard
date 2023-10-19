f0fb1085b78543b7fe6f34c4a069665fc6c49f81
SD-MALINGA <somyaditya@decentralisedinvestment.group>
2023-10-20 01:47:41 +0530

    build issue fixed

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,6 +1,6 @@
 import { Payment, columns } from "./columns";
 import { DataTable } from "@components/ui/data-table";
-import UserPage from "@/components/dashboard/Users/UserPage";
+import UserPage from "@/components/dashboard/users/UserPage";
 
 export default async function DemoPage() {
   return (
