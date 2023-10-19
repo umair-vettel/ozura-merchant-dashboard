e63198876dfb84581786afdf81b69d2eac3459f9
SD-MALINGA <somyaditya@decentralisedinvestment.group>
2023-10-20 01:51:06 +0530

    remove unused import

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,5 +1,3 @@
-import { Payment, columns } from "./columns";
-import { DataTable } from "@components/ui/data-table";
 import UserPage from "@/components/dashboard/users/UserPage";
 
 export default async function DemoPage() {
